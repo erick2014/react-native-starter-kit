eadc4157d2c43b81a8d6bd5e51de63306866e283
erick <erickarmandogarcia@hotmail.com>
2018-01-21 12:31:30 -0500

    Fix indentation in productList file

--- App/components/productList/productList.js
+++ App/components/productList/productList.js
@@ -1,32 +1,32 @@
 import React, { Component } from 'react';
 import {
-	Text,
-	View,
-	StyleSheet
+  Text,
+  View,
+  StyleSheet
 } from 'react-native';
 
 class ProductList extends Component {
-	static navigationOptions = {
-		title: 'Products List'
-	}
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text>
-					Product List component
+  static navigationOptions = {
+    title: 'Products List'
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Product List component
         </Text>
-			</View>
-		);
-	}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: 'center',
-		alignItems: 'center',
-		backgroundColor: '#F5FCFF',
-	}
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  }
 });
 
 export default ProductList;
