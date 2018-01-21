86c6dc99d599c889342090706fd3e52697d38bb2
erick <erickarmandogarcia@hotmail.com>
2018-01-21 13:24:46 -0500

    Add navigation example to go to another view and go back to the previous view

--- App/components/productDetail/productDetail.js
+++ App/components/productDetail/productDetail.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
 
 class ProductDetail extends Component {
+
+  static navigationOptions = {
+    title: 'Products Detail'
+  }
+
   render() {
+    const { state: { params } } = this.props.navigation;
     return (
       <View>
         <Text>
-          Product detail
+          Product Detail - {params.product}
         </Text>
       </View>
     );
--- App/components/productList/productList.js
+++ App/components/productList/productList.js
@@ -2,19 +2,27 @@ import React, { Component } from 'react';
 import {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  Button
 } from 'react-native';
 
 class ProductList extends Component {
+
   static navigationOptions = {
     title: 'Products List'
   }
+
   render() {
+    const { navigate } = this.props.navigation;
     return (
       <View style={styles.container}>
         <Text>
           Product List component
         </Text>
+        <Button
+          onPress={() => { navigate('productDetail', { "product": 'product 1' }) }}
+          title="Go to Detail"
+        />
       </View>
     );
   }
