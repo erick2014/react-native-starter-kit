544829362468a895216c767106a85855b9cd701a
erick <erickarmandogarcia@hotmail.com>
2018-01-21 12:29:19 -0500

    Remove space

--- App/App.js
+++ App/App.js
@@ -1,16 +1,15 @@
 import React, { Component } from 'react';
 import {
-	Text,
-	View
+  Text,
+  View
 } from 'react-native';
 
 import RootNavigator from './components/rootNavigator/rootNavigator';
 
 export default class App extends Component {
-	render() {
-		return (
-			<RootNavigator />
-		);
-	}
+  render() {
+    return (
+      <RootNavigator />
+    );
+  }
 }
-
